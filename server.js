52f784cdcd0080ed0962158cc36297db59a0d902
Pnanam03 <pnaman03@gmail.com>
2021-04-21 00:08:12 +0530

    make it better

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ let options = {
 //db connection
 connectDB();
 
-if (process.env.NODE_ENV === "production") {
-  // Set Static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+// Set Static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 
 //don't show the log when it is test
 if (config.util.getEnv("NODE_ENV") !== "test") {
